e3c269aad1f8f6525d12f6f733c06e401d0136c7
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2017-02-05 19:15:41 +0800

    add read files

--- app/src/main/java/zero/zd/zquestionnaire/LoadQnaActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/LoadQnaActivity.java
@@ -3,8 +3,16 @@ package zero.zd.zquestionnaire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoadQnaActivity extends AppCompatActivity {
+
+    private List<String> mQnaFileList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,23 @@ public class LoadQnaActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        mQnaFileList = new ArrayList<>();
+
+        File folder = new File("/sdcard/ZQuestionnaire/");
+        File files[] = folder.listFiles();
+
+        if (files.length != 0) {
+            for (File f : files) {
+                String name = f.getName();
+                mQnaFileList.add(name);
+            }
+        }
+
+        ListView list = (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mQnaFileList);
+        list.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/zero/zd/zquestionnaire/MainActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/MainActivity.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create a folder
+        File folder = new File("/sdcard/ZQuestionnaire/");
+        if (!folder.exists()) folder.mkdirs();
     }
 
     public void onClickAnswer(View view) {
