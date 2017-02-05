e50791707ddfcbc71edc060a3fe9137b1aa9b85a
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2017-02-05 18:49:50 +0800

    add back button

--- app/src/main/java/zero/zd/zquestionnaire/LoadQnaActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/LoadQnaActivity.java
@@ -2,6 +2,7 @@ package zero.zd.zquestionnaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LoadQnaActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class LoadQnaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_qna);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
