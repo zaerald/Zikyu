f7a2065294c4aa277bc049d5c075e11495f9afab
zd-zero <zaerald.zd@gmail.com>
2017-02-27 02:41:07 +0800

    update UI

--- app/src/main/java/zero/zd/zquestionnaire/QnaBuilderActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/QnaBuilderActivity.java
@@ -2,7 +2,10 @@ package zero.zd.zquestionnaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class QnaBuilderActivity extends AppCompatActivity {
 
@@ -17,13 +20,27 @@ public class QnaBuilderActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_qna_builder, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+
+            case R.id.action_save:
+                break;
+
             case android.R.id.home:
                 finish();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickAdd(View view) {
+        Toast.makeText(QnaBuilderActivity.this, "ADD!!!", Toast.LENGTH_SHORT).show();
     }
 
 }
