c5b7f41cf56baef0185b1a07328c14402b9631d6
zd-zero <zaerald.zd@gmail.com>
2017-04-06 19:17:44 +0800

    :pencil: Remove Unused Imports

--- app/src/main/java/zero/zd/zquestionnaire/MainActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/MainActivity.java
@@ -1,9 +1,8 @@
 package zero.zd.zquestionnaire;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import java.io.File;
