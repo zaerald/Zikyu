f3f440380bd67ec450160f7bc34fa67f6bfa48d5
zd-zero <zaerald.zd@gmail.com>
2017-04-09 22:47:49 +0800

    :speaker: Add Logs on Filed Creation of Folders

--- app/src/main/java/zero/zd/zquestionnaire/MainActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/MainActivity.java
@@ -3,11 +3,14 @@ package zero.zd.zquestionnaire;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
         // create a folder
         File folder = new File(Environment.getExternalStorageDirectory().getPath() + "/ZQuestionnaire/");
-        if (!folder.exists()) folder.mkdirs();
+        if (!folder.exists()) {
+            if (!folder.mkdirs()) {
+                Log.e(TAG, "Failed on creating folders.");
+            }
+        }
     }
 
     public void onClickAnswer(View view) {
