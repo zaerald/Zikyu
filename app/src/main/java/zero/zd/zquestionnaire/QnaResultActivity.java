ba3c0d295ac6e7553aae87a68f636106bb7f2784
zd-zero <zaerald.zd@gmail.com>
2017-04-14 23:28:11 +0800

    Add Result Activity

---
+++ app/src/main/java/zero/zd/zquestionnaire/QnaResultActivity.java
@@ -0,0 +1,13 @@
+package zero.zd.zquestionnaire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QnaResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qna_result);
+    }
+}
