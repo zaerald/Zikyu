ea0c0cf310aec372d3729cad7d10dde524648efa
zd-zero <zaerald.zd@gmail.com>
2017-04-15 01:31:13 +0800

    Add Color on Assessment Text View

--- app/src/main/java/zero/zd/zquestionnaire/QnaResultActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/QnaResultActivity.java
@@ -2,6 +2,7 @@ package zero.zd.zquestionnaire;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,6 +38,13 @@ public class QnaResultActivity extends AppCompatActivity {
         // update assessment message
         TextView textAssessment = (TextView) findViewById(R.id.text_msg_assessment);
         textAssessment.setText(assessment);
+        if (assessment.equalsIgnoreCase("passed!")) {
+            textAssessment.setTextColor(ResourcesCompat
+                    .getColor(getResources(), R.color.assessment_passed, null));
+        } else {
+            textAssessment.setTextColor(ResourcesCompat
+                    .getColor(getResources(), R.color.assessment_failed, null));
+        }
 
         // update result message
         TextView textResult = (TextView) findViewById(R.id.text_msg_result);
