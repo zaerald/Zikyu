1b223c30f73481716d6ce19150e5a27c43503199
zd-zero <zaerald.zd@gmail.com>
2017-04-14 15:11:23 +0800

    Limit QnA to 5

--- app/src/main/java/zero/zd/zquestionnaire/QnAHelper.java
+++ app/src/main/java/zero/zd/zquestionnaire/QnAHelper.java
@@ -18,11 +18,11 @@ class QnAHelper {
         qnaList.add(new QnA("GHI", "GHI"));
         qnaList.add(new QnA("JKL", "JKL"));
         qnaList.add(new QnA("MNO", "MNO"));
-        qnaList.add(new QnA("PQR", "PQR"));
-        qnaList.add(new QnA("STU", "STU"));
-        qnaList.add(new QnA("VWX", "VWX"));
-        qnaList.add(new QnA("YZA", "YZA"));
-        qnaList.add(new QnA("BCD", "BCD"));
+//        qnaList.add(new QnA("PQR", "PQR"));
+//        qnaList.add(new QnA("STU", "STU"));
+//        qnaList.add(new QnA("VWX", "VWX"));
+//        qnaList.add(new QnA("YZA", "YZA"));
+//        qnaList.add(new QnA("BCD", "BCD"));
 
         return qnaList;
     }
