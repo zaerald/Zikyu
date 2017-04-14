d58f111fe1239d086d3fb00d2482426a767dd55b
zd-zero <zaerald.zd@gmail.com>
2017-04-14 23:31:18 +0800

    Add getStartIntent()

--- app/src/main/java/zero/zd/zquestionnaire/QnaResultActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/QnaResultActivity.java
@@ -1,9 +1,15 @@
 package zero.zd.zquestionnaire;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class QnaResultActivity extends AppCompatActivity {
+
+    public static Intent getStartIntent(Context context) {
+        return new Intent(context, QnaResultActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
