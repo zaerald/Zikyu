a9c017743bbe8095d1108725ea6b35ca326f69af
zd-zero <zaerald.zd@gmail.com>
2017-05-09 04:03:07 +0800

    Merge Features

--- app/src/main/java/zero/zd/zquestionnaire/QnaState.java
+++ app/src/main/java/zero/zd/zquestionnaire/QnaState.java
@@ -36,17 +36,14 @@ public class QnaState {
     public void setMistakeQnaList(ArrayList<QnA> mistakeQnaList) {
         mMistakeQnaList = mistakeQnaList;
     }
-<<<<<<< HEAD
 
-//    @Override
-//    public String toString() {
-//        String out = "QNA LIST: \n";
-//        for (QnA qna : mQnAList) {
-//            out += qna.toString() + "\n";
-//        }
-//        out += "END";
-//        return out;
-//    }
-=======
->>>>>>> Add Mistake QnA List
+    @Override
+    public String toString() {
+        String out = "QNA LIST: \n";
+        for (QnA qna : mQnAList) {
+            out += qna.toString() + "\n";
+        }
+        out += "END";
+        return out;
+    }
 }
