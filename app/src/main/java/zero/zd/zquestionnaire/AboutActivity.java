923a1492f01160c98c98a4561307a8ce5fe52a7b
zd-zero <zaerald.zd@gmail.com>
2017-05-13 10:35:54 +0800

    Refactor activity_information to activity_about

--- app/src/main/java/zero/zd/zquestionnaire/AboutActivity.java
+++ app/src/main/java/zero/zd/zquestionnaire/AboutActivity.java
@@ -16,7 +16,7 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_information);
+        setContentView(R.layout.activity_about);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
